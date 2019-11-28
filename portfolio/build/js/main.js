409ed7121bf1467d4cdd9f1fcbca172b73e3bfb9
r0stya <rostyslavPutas@gmail.com>
2019-11-28 14:25:34 +0200

    preloader

---
+++ portfolio/build/js/main.js
@@ -0,0 +1,10 @@
+
+document.body.onload=function(){
+         
+          setTimeout(function(){
+                    var preloader = document.getElementById('preloader');
+                    preloader.classList.add('none');
+                    new WOW().init();
+          }, 1000)
+          
+}
