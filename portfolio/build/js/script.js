a7bb4ec565c5dbe8038171ddd6c7ad6a9ce27c53
r0stya <rostyslavPutas@gmail.com>
2019-11-28 14:31:47 +0200

    preload

--- portfolio/build/js/script.js
+++ portfolio/build/js/script.js
@@ -1,2 +1,11 @@
 
-new WOW().init();
+
+document.body.onload=function(){
+         
+          setTimeout(function(){
+                    var preloader = document.getElementById('preloader');
+                    preloader.classList.add('none');
+                    new WOW().init();
+          }, 1000)
+          
+}
