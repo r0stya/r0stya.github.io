8e38685037bd722fabbe0106163d934e7abec536
r0stya <rostyslavPutas@gmail.com>
2020-01-12 19:29:32 +0200

    w

---
+++ bouncy/src/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+        //   $('.navi__hiden__btn').click(function(){
+        //     $('.navi_hiden').css('display','flex')
+
+        //   })
+        $('.navi__hiden__btn').click(function(){
+            $('.navi_hiden').toggleClass('toggleClick')
+           
+        })
+         
+         
+
+})
